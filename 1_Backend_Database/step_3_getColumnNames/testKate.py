e4e1f64d17ea043b4351a2f0781f1114b56056ba
Alec Horwitz <horwitz_alec@wheatoncollege.edu>
2016-12-07 15:05:59 -0500

    Update testKate.py

--- 1_Backend_Database/step_3_getColumnNames/testKate.py
+++ 1_Backend_Database/step_3_getColumnNames/testKate.py
@@ -3,7 +3,6 @@ import datetime as DT
 from random import randint
 
 dbpath="Kate.db"
-datapath="Kate.csv"
 
 ##Connect to the Database by creating an instance of dataAccess 
 Katedb = DB("Kate",dbpath)
@@ -14,12 +13,8 @@ dict2={0: '"0"'}
 #Here we create two dummy dictionaries to send to the database via addcolumn.
 #This will allow dataAccess.py to know what type it should populate the column with. 
 
-##Give the Database the path to a file to read in
-
-Katedb.insertData(datapath)
-
 names= Katedb.getColumnNames()
-print names, " Before"
+print " Before: ", names
 
 #This is before we add our colums
 
@@ -28,10 +23,6 @@ Katedb.addColumn("randomNums", dict1)
 Katedb.addColumn("stringColumn", dict2)
 # Add in our two new columns!
 
-data = Katedb.getData()
-
 names= Katedb.getColumnNames()
-print names, " After"
-# Now you should see what you added.
-#Note that since the original db file had coulmns 0-7 in place, we will need to update the values of the newly
-# added coulmns 8 and 9
+print "After: ", names
+# Now you should see the names of the columns you added.
